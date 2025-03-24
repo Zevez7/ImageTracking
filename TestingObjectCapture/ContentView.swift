3e7243e37e723cb58f1f1d05280faf730ae70707
Zevez7 <datguy7@gmail.com>
2025-03-24 15:28:33 -0400

    Initial Commit

---
+++ TestingObjectCapture/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestingObjectCapture
+//
+//  Created by Dat Nguyen on 3/24/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
