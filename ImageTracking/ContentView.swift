bf1ade666cea22c90c70fedb4e2995a630497d04
Zevez7 <datguy7@gmail.com>
2025-03-26 00:33:55 -0400

    image recognition to image tracking

--- ImageTracking/ContentView.swift
+++ ImageTracking/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
 
     var body: some View {
        
-        Text("Image Recongnition")
+        Text("Image Tracking")
             .font(.largeTitle)
         
         
